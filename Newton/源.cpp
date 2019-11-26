f7f17a3bb7289600156cadd56dc0d02b18225990
浩毅 陈 <chenhaoyi1@outlook.com>
2019-11-26 11:37:38 +0800

    test

--- Newton/源.cpp
+++ Newton/源.cpp
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 double f1(double x) {
+	x = x * x * x + 2 * x * x + 10 * x - 20;
 	return x;
 }
 double f2(double x) {
+	x = 3 * x * x + 4 * x + 10;
 	return x;
 }
 void newton(double (*f1)(double), double (*f2)(double)) {
 	double x0, x1, e, y; int k = 0, n;
 	printf("Please input x0 and e:");
 	scanf("%lf%lf", &x0, &e);
-	printf("Please input N:");
-	scanf("%d", &n);
-	while (k <= n) {
+	//printf("Please input N:");
+	//scanf("%d", &n);
+	while (true) {
 		y = (*f2)(x0);
 		if (y == 0) { printf("f'(x0)=0\n"); return; }
 		x1 = x0 - (*f1)(x0) / y;
 		k++;
 		if (fabs(x1 - x0) < e && fabs((*f1)(x1) < e)) {
-			printf("x1=%-10.5lf y(x1)=%-10.5lf k=%d\n", x1, (*f1)(x1), k);
+			printf("x1=%-10.6lf y(x1)=%-10.6lf k=%d\n", x1, (*f1)(x1), k);
 			break;
 		}
 		x0=x1;
